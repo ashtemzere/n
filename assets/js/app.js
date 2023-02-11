b970b1bba99a1e028b3ab1acff6b965cd037cbb0
ashtemzere <116907766+ashtemzere@users.noreply.github.com>
2023-02-11 22:51:45 +0300

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,24 @@
-$(function(){var form=$('#ajax-contact');var formMessages=$('#form-messages');$(form).submit(function(e){e.preventDefault();var formData=$(form).serialize();$.ajax({type:'POST',url:$(form).attr('action'),data:formData}).done(function(response){$(formMessages).removeClass('error');$(formMessages).addClass('success');$(formMessages).text(response);$('#name').val('');$('#email').val('');$('#subject').val('');$('#message').val('');}).fail(function(data){$(formMessages).removeClass('success');$(formMessages).addClass('error');if(data.responseText!==''){$(formMessages).text(data.responseText);}else{$(formMessages).text('Oops! An error occured and your message could not be sent.');}});});});
+// Dom7
+var $$ = Dom7;
+
+var app = new Framework7({
+    // App root element
+    root: '#app',
+    // App Name
+    name: 'CyrusHub',
+    // App id
+    id: 'com.myapp.test',
+    // Theme
+    theme: 'ios',
+    // Enable swipe panel
+    panel: {
+      swipe: 'left',
+    },
+    // Add default routes
+    routes: routes,
+    // ... other parameters
+  });
+  
+  var mainView = app.views.create('.view-main', {
+    url: '/'
+  });
